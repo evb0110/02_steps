b7486c959d2860e938a97cb6b1d99c68c0b1a676
evb0110 <7149553@gmail.com>
2019-07-26 22:02:48 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import StepsForm from './components/StepsForm';
+
 function App() {
   return (
     <div className="App">
-
+      <StepsForm />
     </div>
   );
 }
