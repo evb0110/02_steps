647e9ac20ba88cb6f44943f84e826e5d4f9590e2
evb0110 <7149553@gmail.com>
2019-07-27 15:03:32 +0300

    forward

--- src/components/StepsList.js
+++ src/components/StepsList.js
@@ -4,19 +4,24 @@ import StepsRow from './StepsRow';
 import './StepList.css';
 
 export default function StepsList(props) {
-  const handleDelete = (id) => props.onDelete(id);
+  const handleDelete = id => props.onDelete(id);
   return (
-    <table>
-      <tbody>
-        <tr>
-          <th>Дата (ДД.ММ.ГГ)</th>
-          <th>Пройдено км</th>
-          <th>Действия</th>
-        </tr>
-        {props.walks.map(walk => (
-          <StepsRow handleDelete={handleDelete} key={walk.id} id={walk.id} walk={walk} />
-        ))}
-      </tbody>
-    </table>
+      <table>
+        <tbody>
+          <tr>
+            <th>Дата (ДД.ММ.ГГ)</th>
+            <th>Пройдено км</th>
+            <th>Действия</th>
+          </tr>
+          {props.walks.map(walk => (
+            <StepsRow
+              handleDelete={handleDelete}
+              key={walk.id}
+              id={walk.id}
+              walk={walk}
+            />
+          ))}
+        </tbody>
+      </table>
   );
 }
