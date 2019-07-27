0ccfd1f5d5f6e718a694c20bb60e244e3ab13f60
evb0110 <7149553@gmail.com>
2019-07-27 18:17:53 +0300

    added proptypes

--- src/components/StepsRow.js
+++ src/components/StepsRow.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import './StepsRow.css'
+import PropTypes from 'prop-types';
+import './StepsRow.css';
 
 export default function StepsRow({ walk, handleDelete }) {
-  const onDelete = () => handleDelete(walk.id)
+  const onDelete = () => handleDelete(walk.id);
   return (
     <tr key={walk.id}>
       <td className="container-date">{walk.date}</td>
       <td className="container-km">{walk.km}</td>
       <td className="container-delete">
-        <i class="material-icons" onClick={onDelete}><i class="material-icons">
-        delete_forever
-        </i></i>
+        <i class="material-icons" onClick={onDelete}>
+          <i class="material-icons">delete_forever</i>
+        </i>
       </td>
     </tr>
   );
 }
+
+StepsRow.propTypes = {
+  walk: PropTypes.shape({
+    id: PropTypes.string,
+    date: PropTypes.string,
+    km: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  }),
+  handleDelete: PropTypes.func,
+};
